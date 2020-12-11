f1dba53ba363a90c631f5e3b0ad6c9a2e440bc04
DevWabad <56509834+DevWabad@users.noreply.github.com>
2020-12-11 20:07:37 +0800

    add Products its table

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Paper from '@material-ui/core/Paper';
 
 function Dashboard() {
     return (
--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import MaterialTable from 'material-table'
+import { tableIcons, tablePageSizeoptions } from '../../utils/utils';
 
-function Products() {
+import { CategoryContext } from '../../contexts/CategoryContext'
+import { ProductContext } from '../../contexts/ProductContext'
+import { SizeContext } from '../../contexts/SizeCotext'
+
+
+function Products(props) {
+
+    const { products, productsLoading } = useContext(ProductContext)
+    const { sizes } = useContext(SizeContext)
+    const { categories } = useContext(CategoryContext)
+
+    const sizeLookup = {}
+    const categoryLookup = {}
+
+    useEffect(() => {
+        sizes.map(size => {
+            sizeLookup[size.id] = size.name
+        })
+
+        categories.map(category => {
+            categoryLookup[category.id] = category.name
+        })
+
+    }, [])
+
+    const [columns, setColumns] = useState([
+        { title: 'ID', field: 'id', editable: 'never' },
+        { title: 'Product Names', field: 'name' },
+        { title: 'Sizes', field: 'size', lookup: sizeLookup},
+        { title: 'Categories', field: 'category',lookup: categoryLookup },
+        { title: 'Prices', field: 'price' },
+        { title: 'Created On', field: 'created_at', editable: 'never' },
+    ])
+
     return (
-        <div>
-            Products
-        </div>
+        <MaterialTable
+            isLoading={productsLoading}
+            icons={tableIcons}
+            options={tablePageSizeoptions}
+            title="Products"
+            columns={columns}
+            data={products}
+            editable={{
+                onRowAdd: newData =>
+                    new Promise(async (resolve, reject) => {
+                        console.log(newData)
+                        reject()
+                    }),
+                onRowUpdate: (newData, oldData) =>
+                    new Promise(async (resolve, reject) => {
+                        reject()
+                    }),
+                onRowDelete: (oldData) =>
+                    new Promise(async (resolve, reject) => {
+                        reject()
+                    })
+            }}
+        />
     )
 }
 
