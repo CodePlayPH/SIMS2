dcfd4a7928d21029739c96da24a6e76063141c98
DevWabad <56509834+DevWabad@users.noreply.github.com>
2021-01-10 20:25:30 +0800

    Entry UI edited

--- src/pages/entries/Entries.js
+++ src/pages/entries/Entries.js
@@ -1,16 +1,16 @@
 import MaterialTable from "material-table";
 import React, { useContext, useEffect, useState } from "react";
 
-import MenuItem from '@material-ui/core/MenuItem';
-import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import Button from '@material-ui/core/Button';
-
+import MenuItem from "@material-ui/core/MenuItem";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import Button from "@material-ui/core/Button";
 
 import { tableIcons, tablePageSizeoptions } from "../../utils/utils";
+import "./styles.scss";
 
 //contexts
 import { CategoryContext } from "../../contexts/CategoryContext";
@@ -18,216 +18,229 @@ import { ProductContext } from "../../contexts/ProductContext";
 import { SizeContext } from "../../contexts/SizeCotext";
 
 const useStyles = makeStyles((theme) => ({
-
-    formlayout: {
-        marginLeft: 200,
-    },
-
-    selectlabel: {
-        marginBottom: theme.spacing(10),
-    }, root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: '25ch',
-        },
-    },
-
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: "100%",
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
 }));
 
-
 function NewEntries() {
+  const classes = useStyles();
 
-    const [Category, setCategory] = React.useState('');
+  const [Category, setCategory] = React.useState("");
 
-    const handleChange = (event) => {
-        setCategory(event.target.value);
-    };
+  const handleChange = (event) => {
+    setCategory(event.target.value);
+  };
 
-    const [Product, setProduct] = React.useState('');
+  const [Product, setProduct] = React.useState("");
 
-    const handleChangeProduct = (event) => {
-        setProduct(event.target.value);
-    };
+  const handleChangeProduct = (event) => {
+    setProduct(event.target.value);
+  };
 
+  const { fetchCategories } = useContext(CategoryContext);
+  const { fetchSizes } = useContext(SizeContext);
+  const { fetchProducts } = useContext(ProductContext);
 
-    const { fetchCategories } = useContext(CategoryContext)
-    const { fetchSizes } = useContext(SizeContext)
-    const { fetchProducts } = useContext(ProductContext)
+  useEffect(() => {
+    Promise.all([fetchCategories(), fetchSizes()]).then((values) => {
+      fetchProducts();
+      console.log("akoa ning categorisss" + fetchCategories());
+    });
+  }, []);
 
-    useEffect(() => {
-        Promise.all([fetchCategories(), fetchSizes()]).then((values) => {
-            fetchProducts()
-            console.log("akoa ning categorisss"+fetchCategories());
-        });
-    }, [])
- 
+  //table
 
-    //table
+  const { products, productsLoading } = useContext(ProductContext);
+  const { sizes } = useContext(SizeContext);
+  const { categories } = useContext(CategoryContext);
 
-    const { products, productsLoading } = useContext(ProductContext);
-    const { sizes } = useContext(SizeContext);
-    const { categories } = useContext(CategoryContext);
+  const [product_name, setProduct_name] = useState("");
+  const [product_price, setProduct_price] = useState("");
+  const [size_id, setSize_id] = useState("");
+  const [category_id, setCategory_id] = useState("");
+  const { addProduct } = useContext(ProductContext);
 
-    const [product_name, setProduct_name] = useState("");
-    const [product_price, setProduct_price] = useState("");
-    const [size_id, setSize_id] = useState("");
-    const [category_id, setCategory_id] = useState("");
-    const { addProduct } = useContext(ProductContext);
+  const sizeLookup = {};
+  const categoryLookup = {};
 
-    const sizeLookup = {};
-    const categoryLookup = {};
+  const handleAddNow = async (event) => {
+    event.preventDefault();
+    setProduct_name(columns.name);
+    setProduct_price(columns.price);
+    setSize_id(columns.size);
+    setCategory_id(columns.category);
 
-    const handleAddNow = async (event) => {
-        event.preventDefault();
-        setProduct_name(columns.name);
-        setProduct_price(columns.price);
-        setSize_id(columns.size);
-        setCategory_id(columns.category);
+    await addProduct({ product_name, product_price, size_id, category_id });
+  };
 
-        await addProduct({ product_name, product_price, size_id, category_id });
-    };
+  useEffect(() => {
+    sizes.map((size) => {
+      sizeLookup[size.id] = size.name;
+    });
 
-    useEffect(() => {
-        sizes.map((size) => {
-            sizeLookup[size.id] = size.name;
-        });
+    categories.map((category) => {
+      categoryLookup[category.id] = category.name;
+    });
+  }, []);
 
-        categories.map((category) => {
-            categoryLookup[category.id] = category.name;
-        });
-    }, []);
+  const [columns, setColumns] = useState([
+    { title: "ID", field: "id", editable: "never" },
+    { title: "Product Name", field: "name" },
+    { title: "Size", field: "size", lookup: sizeLookup },
+    { title: "Category", field: "category", lookup: categoryLookup },
+    { title: "Price", field: "price" },
+    { title: "Date Created", field: "created_at", editable: "never" },
+  ]);
 
-    const [columns, setColumns] = useState([
-        { title: "ID", field: "id", editable: "never" },
-        { title: "Product Name", field: "name" },
-        { title: "Size", field: "size", lookup: sizeLookup },
-        { title: "Category", field: "category", lookup: categoryLookup },
-        { title: "Price", field: "price" },
-        { title: "Date Created", field: "created_at", editable: "never" },
-    ]);
+  //table
 
+  return (
+    <div className="container-fluid entry-main-container">
+      <div className="row entry-title-row">
+        <h1>Entries</h1>
+      </div>
 
-    //table
-
-    return (
-        <div style={{ flex: 50 }}>
-            <h1>Dashboard</h1>
-
-
-            <div style={{ marginTop: 50, marginLeft: 50 }}>
-
-                <div style={{ marginTop: 10, marginBottom: 30 }}>
-                    <label>
-                        Entry Number:
-                            <input type="text" name="name" />
-                    </label>
-                </div>
-
-                <FormControl variant="outlined">
-                    <InputLabel id="demo-simple-select-outlined-label" className={useStyles.selectlabel}>Category</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-outlined-label"
-                        id="demo-simple-select-outlined"
-                        value={Category}
-                        onChange={handleChange}
-                        label="Category"
-                    >
-                        <MenuItem value="Frozen Goods1">Frozen Goods1</MenuItem>
-                        <MenuItem value="Frozen Goods2">Frozen Goods2</MenuItem>
-                        <MenuItem value="Frozen Goods3">Frozen Goods3</MenuItem>
-                        <MenuItem value="Frozen Goods4">Frozen Goods4</MenuItem>
-                    </Select>
-                </FormControl>
-
-                <FormControl variant="outlined" style={{ marginLeft: 100 }}>
-                    <InputLabel id="demo-simple-select-outlined-label" className={useStyles.selectlabel}>Product Name: </InputLabel>
-                    <Select
-                        labelId="demo-simple-select-outlined-label"
-                        id="demo-simple-select-outlined"
-                        value={Product}
-                        onChange={handleChangeProduct}
-                        label="Product Name: "
-                    >
-                        <MenuItem value="Frozen Goods1">Frozen Goods1</MenuItem>
-                        <MenuItem value="Frozen Goods2">Frozen Goods2</MenuItem>
-                        <MenuItem value="Frozen Goods3">Frozen Goods3</MenuItem>
-                        <MenuItem value="Frozen Goods4">Frozen Goods4</MenuItem>
-                    </Select>
-                </FormControl>
-
-                <FormControl variant="outlined" style={{ marginLeft: 100 }}>
-                    <InputLabel id="demo-simple-select-outlined-label" className={useStyles.selectlabel}>Category</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-outlined-label"
-                        id="demo-simple-select-outlined"
-                        value={Category}
-                        onChange={handleChange}
-                        label="Category"
-                    >
-                        <MenuItem value="Frozen Goods1">Frozen Goods1</MenuItem>
-                        <MenuItem value="Frozen Goods2">Frozen Goods2</MenuItem>
-                        <MenuItem value="Frozen Goods3">Frozen Goods3</MenuItem>
-                        <MenuItem value="Frozen Goods4">Frozen Goods4</MenuItem>
-                    </Select>
-                </FormControl>
-
-
-
-
-            </div>
-
-            <TextareaAutosize style={{ marginTop: 40, marginLeft: 50, width: '50%', }} aria-label="minimum height" rowsMin={6} placeholder="Minimum 3 rows" />
-
-            <div style={{ marginTop: 50, marginLeft: 50, display: 'flex' }}>
-
-                <Button style={{ margin: 10 }} variant="contained">Cancel</Button>
-
-                <Button style={{ margin: 10 }} variant="contained" color="primary">
-                    Add Item/s
-                </Button>
-            </div>
-
-
-{/*Table*/}
-            <MaterialTable
-                isLoading={productsLoading}
-                icons={tableIcons}
-                options={tablePageSizeoptions}
-                title="Products"
-                columns={columns}
-                data={products}
-
-                editable={{
-                    onRowAdd: (newData) =>
-
-                        new Promise(async (resolve, reject) => {
-
-
-                            console.log("New data: " + columns.name + "; " + columns.price);
-
-                            // await addProduct({
-                            //   product_name,
-                            //   product_price,
-                            //   size_id,
-                            //   category_id,
-                            // });
-                            reject();
-                        }),
-                    onRowUpdate: (newData, oldData) =>
-                        new Promise(async (resolve, reject) => {
-                            reject();
-                            // handleAddNow.bind(this)
-                        }),
-                    onRowDelete: (oldData) =>
-                        new Promise(async (resolve, reject) => {
-                            reject();
-                        }),
-                }}
-            />
-
-
+      <div className="row entry-number-row">
+        <div>
+          <label>
+            Entry Number:
+            <input type="text" disabled name="name" />
+          </label>
         </div>
-    )
+      </div>
+
+      <div className="row entry-select-input">
+        <div className="col-sm"></div>
+        <div className="col-sm">
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel
+              id="demo-simple-select-outlined-label"
+              className="entry-select"
+            >
+              Category
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+              value={Category}
+              onChange={handleChange}
+              label="Category"
+            >
+              {categories.map((category) => {
+                categoryLookup[category.id] = category.name;
+                <MenuItem value={category.id}>{categoryLookup}</MenuItem>;
+              })}
+
+              {/* <MenuItem value="Frozen Goods2">Frozen Goods2</MenuItem>
+            <MenuItem value="Frozen Goods3">Frozen Goods3</MenuItem>
+            <MenuItem value="Frozen Goods4">Frozen Goods4</MenuItem> */}
+            </Select>
+          </FormControl>
+        </div>
+        <div className="col-sm">
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel
+              id="demo-simple-select-outlined-label"
+              className="entry-select"
+            >
+              Product Name:{" "}
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+              value={Product}
+              onChange={handleChangeProduct}
+              label="Product Name: "
+            >
+              <MenuItem value="Frozen Goods1">Frozen Goods1</MenuItem>
+              <MenuItem value="Frozen Goods2">Frozen Goods2</MenuItem>
+              <MenuItem value="Frozen Goods3">Frozen Goods3</MenuItem>
+              <MenuItem value="Frozen Goods4">Frozen Goods4</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="col-sm">
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel
+              id="demo-simple-select-outlined-label"
+              className="entry-select"
+            >
+              Category
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-outlined-label"
+              id="demo-simple-select-outlined"
+              value={Category}
+              onChange={handleChange}
+              label="Category"
+            >
+              <MenuItem value="Frozen Goods1">Frozen Goods1</MenuItem>
+              <MenuItem value="Frozen Goods2">Frozen Goods2</MenuItem>
+              <MenuItem value="Frozen Goods3">Frozen Goods3</MenuItem>
+              <MenuItem value="Frozen Goods4">Frozen Goods4</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+        <div className="col-sm"></div>
+      </div>
+
+      <div className="row entry-text-area">
+        {/* <div className="col"> */}
+        <TextareaAutosize
+          className="txt-area"
+          aria-label="minimum height"
+          rowsMin={6}
+          placeholder="Minimum 3 rows"
+        />
+        {/* </div> */}
+      </div>
+
+      <div className="container btns-container">
+        <div className="col-sm">
+          <Button variant="contained" className="btn">
+            Cancel
+          </Button>
+          <Button variant="contained" color="primary" className="btn">
+            Add Item/s
+          </Button>
+        </div>
+        {/* <div className="col-sm">
+         
+        </div> */}
+      </div>
+
+      {/*Table*/}
+      <MaterialTable
+        isLoading={productsLoading}
+        icons={tableIcons}
+        options={tablePageSizeoptions}
+        title="Products"
+        columns={columns}
+        data={products}
+        editable={{
+          onRowAdd: (newData) =>
+            new Promise(async (resolve, reject) => {
+              console.log("New data: " + columns.name + "; " + columns.price);
+              reject();
+            }),
+          onRowUpdate: (newData, oldData) =>
+            new Promise(async (resolve, reject) => {
+              reject();
+            }),
+          onRowDelete: (oldData) =>
+            new Promise(async (resolve, reject) => {
+              reject();
+            }),
+        }}
+      />
+    </div>
+  );
 }
 
-export default NewEntries
+export default NewEntries;
